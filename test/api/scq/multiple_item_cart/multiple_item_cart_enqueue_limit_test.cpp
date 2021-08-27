b17a697609d556dd90b9e5bc6cfe62692d153d28
marehr <marehr-github@marehr.dialup.fu-berlin.de>
2021-08-27 11:27:58 +0200

    test: multiple_producer_multiple_consumer_[all_full|mixed]_carts

--- test/api/scq/multiple_item_cart/multiple_item_cart_enqueue_limit_test.cpp
+++ test/api/scq/multiple_item_cart/multiple_item_cart_enqueue_limit_test.cpp
@@ -642,3 +642,238 @@ TEST(multiple_item_cart_enqueue_limit_test, multiple_producer_single_consumer_mi
     EXPECT_TRUE(expected.empty());
 }
 
+TEST(multiple_item_cart_enqueue_limit_test, multiple_producer_multiple_consumer_all_full_carts)
+{
+    using value_type = int;
+
+    scq::slotted_cart_queue<value_type> queue{scq::slot_count{3}, scq::cart_count{3}, scq::cart_capacity{2}};
+
+    // expected set contains all (expected) results; after the test which set should be empty (each matching result will
+    // be crossed out)
+    concurrent_cross_off_list<std::pair<std::size_t, value_type>> expected
+    {
+        {0, value_type{001}},
+        {0, value_type{002}}, // full cart 1
+        {1, value_type{100}},
+        {1, value_type{101}}, // full cart 2
+        {1, value_type{102}},
+        {1, value_type{103}}, // full cart 3
+        {2, value_type{200}},
+        {2, value_type{201}}, // full cart 4
+        {2, value_type{202}},
+        {2, value_type{203}} // full cart 5
+    };
+
+    std::mutex enqueue_count_mutex{};
+    std::condition_variable enqueue_count_cv{};
+    std::atomic_size_t enqueue_count{};
+
+    // initialise 10 producing threads
+    std::vector<std::thread> enqueue_threads(10);
+    std::generate(enqueue_threads.begin(), enqueue_threads.end(), [&]()
+    {
+        static size_t thread_id = 0;
+        return std::thread([thread_id = thread_id++, &queue, &enqueue_count, &enqueue_count_cv]
+        {
+            switch (thread_id)
+            {
+                case 0:
+                    queue.enqueue(scq::slot_id{0}, value_type{001});
+                    break;
+                case 1:
+                    queue.enqueue(scq::slot_id{0}, value_type{002});
+                    break;
+                case 2:
+                    queue.enqueue(scq::slot_id{1}, value_type{100});
+                    break;
+                case 3:
+                    queue.enqueue(scq::slot_id{1}, value_type{101});
+                    break;
+                case 4:
+                    queue.enqueue(scq::slot_id{2}, value_type{200});
+                    break;
+                case 5:
+                    queue.enqueue(scq::slot_id{1}, value_type{103});
+                    break;
+                case 6:
+                    queue.enqueue(scq::slot_id{1}, value_type{102});
+                    break;
+                case 7:
+                    queue.enqueue(scq::slot_id{2}, value_type{201});
+                    break;
+                case 8:
+                    queue.enqueue(scq::slot_id{2}, value_type{202});
+                    break;
+                case 9:
+                    queue.enqueue(scq::slot_id{2}, value_type{203});
+                    break;
+            }
+
+            ++enqueue_count;
+            enqueue_count_cv.notify_one();
+        });
+    });
+
+    // wait until at least 6 inserts, after that all other inserts should block
+    {
+        std::unique_lock<std::mutex> enqueue_count_lock(enqueue_count_mutex);
+        enqueue_count_cv.wait(enqueue_count_lock, [&]
+        {
+            return enqueue_count.load() >= 6;
+        });
+    }
+
+    // queue should block on 7th insertion (max cart_count is just 3)
+    // i.e. enqueue_count stays at 6
+    std::this_thread::sleep_for(wait_time);
+    EXPECT_EQ(enqueue_count.load(), 6);
+
+    // concurrently dequeue
+    std::vector<std::thread> dequeue_threads(5);
+    std::generate(dequeue_threads.begin(), dequeue_threads.end(), [&]()
+    {
+        return std::thread([&queue, &expected]
+        {
+            scq::cart<value_type> cart = queue.dequeue();
+            EXPECT_TRUE(cart.valid());
+            std::pair<scq::slot_id, std::span<value_type>> cart_data = cart.get();
+
+            EXPECT_EQ(cart_data.second.size(), 2u);
+
+            for (auto && value: cart_data.second)
+            {
+                EXPECT_TRUE(expected.cross_off({cart_data.first.slot_id, value}));
+            }
+        });
+    });
+
+    for (auto && enqueue_thread: enqueue_threads)
+        enqueue_thread.join();
+
+    EXPECT_EQ(enqueue_count.load(), 10);
+
+    for (auto && dequeue_thread: dequeue_threads)
+        dequeue_thread.join();
+
+    // all results seen
+    EXPECT_TRUE(expected.empty());
+}
+
+TEST(multiple_item_cart_enqueue_limit_test, multiple_producer_multiple_consumer_mixed_carts)
+{
+    using value_type = int;
+
+    scq::slotted_cart_queue<value_type> queue{scq::slot_count{3}, scq::cart_count{3}, scq::cart_capacity{2}};
+
+    // expected set contains all (expected) results; after the test which set should be empty (each matching result will
+    // be crossed out)
+    concurrent_cross_off_list<std::pair<std::size_t, value_type>> expected
+    {
+        {0, value_type{001}}, // non-full cart 1
+        {1, value_type{100}},
+        {1, value_type{101}}, // full cart 1
+        {1, value_type{102}}, // non-full cart 2
+        {2, value_type{200}},
+        {2, value_type{201}}, // full cart 2
+        {2, value_type{202}}, // non-full cart 3
+    };
+
+    std::mutex enqueue_count_mutex{};
+    std::condition_variable enqueue_count_cv{};
+    std::atomic_size_t enqueue_count{};
+
+    // initialise 7 producing threads
+    std::vector<std::thread> enqueue_threads(7);
+    std::generate(enqueue_threads.begin(), enqueue_threads.end(), [&]()
+    {
+        static size_t thread_id = 0;
+        return std::thread([thread_id = thread_id++, &queue, &enqueue_count, &enqueue_count_cv]
+        {
+            switch (thread_id)
+            {
+                case 0:
+                    queue.enqueue(scq::slot_id{0}, value_type{001});
+                    break;
+                case 1:
+                    queue.enqueue(scq::slot_id{1}, value_type{100});
+                    break;
+                case 2:
+                    queue.enqueue(scq::slot_id{1}, value_type{101});
+                    break;
+                case 3:
+                    queue.enqueue(scq::slot_id{2}, value_type{200});
+                    break;
+                case 4:
+                    queue.enqueue(scq::slot_id{1}, value_type{102});
+                    break;
+                case 5:
+                    queue.enqueue(scq::slot_id{2}, value_type{201});
+                    break;
+                case 6:
+                    queue.enqueue(scq::slot_id{2}, value_type{202});
+                    break;
+            }
+
+            ++enqueue_count;
+            enqueue_count_cv.notify_one();
+        });
+    });
+
+    // wait until after 4 inserts; it can be 4 or 5 inserts
+    // 4: slot 0 [1/2], slot 1 [1/2], slot 1 [2/2], slot [1/2], all enqueue's after that block
+    // 5: slot 1 [1/2], slot 1 [2/2], slot 2 [1/2], slot 2 [2/2], slot 0 [1/2], all enqueue's after that block
+    {
+        std::unique_lock<std::mutex> enqueue_count_lock(enqueue_count_mutex);
+        enqueue_count_cv.wait(enqueue_count_lock, [&]
+        {
+            return enqueue_count.load() >= 4u;
+        });
+    }
+
+    // queue should block on 5th or 6th insertion (max cart_count is just 3)
+    // i.e. enqueue_count stays between 5 and 6
+    std::this_thread::sleep_for(wait_time);
+    EXPECT_GE(enqueue_count.load(), 4u);
+    EXPECT_LE(enqueue_count.load(), 5u);
+
+    std::atomic_size_t full_cart_count{};
+    std::atomic_size_t half_filled_cart_count{};
+
+    // concurrently dequeue
+    std::vector<std::thread> dequeue_threads(5);
+    std::generate(dequeue_threads.begin(), dequeue_threads.end(), [&]()
+    {
+        return std::thread([&queue, &expected, &full_cart_count, &half_filled_cart_count]
+        {
+            scq::cart<value_type> cart = queue.dequeue();
+            EXPECT_TRUE(cart.valid());
+            std::pair<scq::slot_id, std::span<value_type>> cart_data = cart.get();
+
+            EXPECT_GE(cart_data.second.size(), 1u);
+            EXPECT_LE(cart_data.second.size(), 2u);
+
+            full_cart_count += cart_data.second.size() == 2u;
+            half_filled_cart_count += cart_data.second.size() == 1u;
+
+            for (auto && value: cart_data.second)
+            {
+                EXPECT_TRUE(expected.cross_off({cart_data.first.slot_id, value}));
+            }
+        });
+    });
+
+    for (auto && enqueue_thread: enqueue_threads)
+        enqueue_thread.join();
+    queue.close();
+
+    EXPECT_EQ(enqueue_count.load(), 7u);
+
+    for (auto && dequeue_thread: dequeue_threads)
+        dequeue_thread.join();
+
+    EXPECT_EQ(full_cart_count.load(), 2u);
+    EXPECT_EQ(half_filled_cart_count.load(), 3u);
+
+    // all results seen
+    EXPECT_TRUE(expected.empty());
+}
